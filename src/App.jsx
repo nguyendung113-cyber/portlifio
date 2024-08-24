eeaf8e99342e40e03dcd25adce54cc4ed3a3e041
nguyendung113-cyber <54460218+nguyendung113-cyber@users.noreply.github.com>
2024-08-24 21:53:55 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { Navbar } from './component/Navbar/navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  return(
 
-  return (
-    <>
-     <h1 className="tw-text-3xl tw-font-bold tw-underline">
-      Hello world!
-    </h1>
-    </>
+    <div className='tw-bg-black'>
+          <Navbar/>
+
+    </div>
   )
 }
 
