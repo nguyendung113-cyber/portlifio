136fb6f237e796af9eec4e65990aef27148f8b87
nguyendung113-cyber <nadung.hutech@gmail.com>
2026-04-26 17:11:50 +0700

    update my portfolio

--- src/component/AboutMe/Aboutme.jsx
+++ src/component/AboutMe/Aboutme.jsx
@@ -1,44 +1,52 @@
+import React from "react";
+import me from "../../assets/image/me.png";
+import myCV from "../../data/myCV.pdf";
 
-import React from 'react';
-import me from '../../assets/image/me.png';
-import myCV from '../../data/myCV.pdf';
-
-export const  Aboutme = () => {
-    return(
-        <section id="Aboutme">
-
-        <div data-bs-theme="dark">
-            <div className=" tw-px-5 tw-flex tw-justify-start tw-w-full"> 
-                <div className="container-fluid" style={{ maxWidth: 1280}}>
-                    <h1 className="tw-text-4xl max-md:tw-text-xs tw-my-4  tw-text-white">Web Developer</h1>
-                </div>
-            </div>
-            <div className="container-fluid" style={{ maxWidth: 1280}}>
-                <div className='tw-flex max-[800px]:tw-flex-col'>
-                    <div className=' tw-flex tw-flex-col max-[400px]:tw-w-[400px]  max-sm:tw-w-full tw-justify-center tw-px-5 tw-py-4 tw-gap-5'>
-                        <h1 className='tw-text-white max-md:tw-text-xl max-sm:tw-text-xs tw-text-4xl'>Xin chào, tôi là  <span className=' tw-text-orange-300 max-md:tw-text-xl max-sm:tw-text-xs tw-text-4xl'>Nguyễn Anh Dũng</span></h1>
-                        <p className='tw-text-white   max-md:tw-text-sm max-[600px]:tw-w-[400px]   tw-text-xl tw-mt-5'> 
-                            Xin chào, tôi là Nguyễn Anh Dũng. Từ khi còn là học sinh, tôi đã có sự tò mò và thích thú đối với máy tính và công nghệ. Tôi đã tốt nghiệp tại trường Đại học Công nghệ TP. Hồ Chí Minh. Tôi mong muốn trở thành một Full-stack Developer và đóng góp vào các dự án lớn, mang lại giá trị cho công ty, bản thân và cồng đồng.
-                        </p>
-                        <p className='tw-text-white max-[600px]:tw-w-[400px]  tw-text-xl tw-mt-5'> 
-                            Châm ngôn của tôi là: "Không ngừng cố gắng, không ngừng học hỏi"
-                        </p>
-                        <a href={myCV} download='CV-Nguyễn Anh Dũng - Intern WEB-DEVELOPER.pdf' >
-                            <button class="tw-h-[80px] tw-text-2xl   tw-text-white tw-w-max tw-bg-gray-800 tw-hover:bg-gray-900 tw-focus:outline-none tw-focus:ring-4 tw-focus:ring-gray-300 tw-font-medium tw-rounded-full tw-px-5 tw-py-2.5 tw-me-2 tw-mb-2 tw-dark:bg-gray-800 tw-dark:hover:bg-gray-700 tw-dark:focus:ring-gray-700 tw-dark:border-gray-700">Download CV</button>
-                        </a>
-                    </div>
-                        <div className=' tw-py-6 max-[800px]:tw-flex max-[800px]:tw-justify-center tw-gap-5'>
-                            <img src={me} alt="Logo" />
-                        </div>
-                </div>
-            </div>
+export const Aboutme = () => {
+  return (
+    <section id="Aboutme">
+      <div data-bs-theme="dark">
+        <div className=" tw-px-5 tw-flex tw-justify-start tw-w-full">
+          <div className="container-fluid" style={{ maxWidth: 1280 }}>
+            <h1 className="tw-text-4xl max-md:tw-text-xs tw-my-4  tw-text-white">
+              Web Developer
+            </h1>
+          </div>
         </div>
-        </section>
-
-            
-);
-}
-
-
-
-
+        <div className="container-fluid" style={{ maxWidth: 1280 }}>
+          <div className="tw-flex max-[800px]:tw-flex-col">
+            <div className=" tw-flex tw-flex-col max-[400px]:tw-w-[400px]  max-sm:tw-w-full tw-justify-center tw-px-5 tw-py-4 tw-gap-5">
+              <h1 className="tw-text-white max-md:tw-text-xl max-sm:tw-text-xs tw-text-4xl">
+                Xin chào, tôi là{" "}
+                <span className=" tw-text-orange-300 max-md:tw-text-xl max-sm:tw-text-xs tw-text-4xl">
+                  Nguyễn Anh Dũng
+                </span>
+              </h1>
+              <p className="tw-text-white   max-md:tw-text-sm max-[600px]:tw-w-[400px]   tw-text-xl tw-mt-5">
+                Xin chào, tôi là Nguyễn Anh Dũng. Từ khi còn là học sinh, tôi đã
+                có sự tò mò và thích thú đối với máy tính và công nghệ. Tôi đã
+                tốt nghiệp tại trường Đại học Công nghệ TP. Hồ Chí Minh. Tôi
+                mong muốn trở thành một Full-stack Developer và đóng góp vào các
+                dự án lớn, mang lại giá trị cho công ty, bản thân và cồng đồng.
+              </p>
+              <p className="tw-text-white max-[600px]:tw-w-[400px]  tw-text-xl tw-mt-5">
+                Châm ngôn của tôi là: "Không ngừng cố gắng, không ngừng học hỏi"
+              </p>
+              <a
+                href={myCV}
+                download="CV-Nguyễn Anh Dũng - Fullstack WEB-DEVELOPER.pdf"
+              >
+                <button class="tw-h-[80px] tw-text-2xl   tw-text-white tw-w-max tw-bg-gray-800 tw-hover:bg-gray-900 tw-focus:outline-none tw-focus:ring-4 tw-focus:ring-gray-300 tw-font-medium tw-rounded-full tw-px-5 tw-py-2.5 tw-me-2 tw-mb-2 tw-dark:bg-gray-800 tw-dark:hover:bg-gray-700 tw-dark:focus:ring-gray-700 tw-dark:border-gray-700">
+                  Download CV
+                </button>
+              </a>
+            </div>
+            <div className=" tw-py-6 max-[800px]:tw-flex max-[800px]:tw-justify-center tw-gap-5">
+              <img src={me} alt="Logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/component/Timeline/Timeline.jsx
+++ src/component/Timeline/Timeline.jsx
@@ -1,90 +1,128 @@
-import React from 'react';
+import React from "react";
 
 const Timeline = () => {
-
-
   return (
     <section>
-
-<div>
-<div className=" tw-text-center tw-w-full">
-                <div className=" tw-flex ">
-                    <div className=" tw-border-solid tw-m-auto  tw-border-l tw-w-full ">
-                        <div className="border-solid border-1 tw-flex tw-items-center tw-border-yellow-200"></div>
-                    </div>
-                    <h1 className="tw-text-4xl tw-w-[500px] tw-my-4 tw-text-white">Thành tựu</h1>
-                    <div className=" tw-border-solid tw-m-auto  tw-border-l tw-w-full ">
-                        <div className="border-solid border-1 tw-flex tw-items-center tw-border-yellow-200"></div>
-                    </div>
+      <div>
+        <div className=" tw-text-center tw-w-full">
+          <div className=" tw-flex ">
+            <div className=" tw-border-solid tw-m-auto  tw-border-l tw-w-full ">
+              <div className="border-solid border-1 tw-flex tw-items-center tw-border-yellow-200"></div>
+            </div>
+            <h1 className="tw-text-4xl tw-w-[500px] tw-my-4 tw-text-white">
+              Thành tựu
+            </h1>
+            <div className=" tw-border-solid tw-m-auto  tw-border-l tw-w-full ">
+              <div className="border-solid border-1 tw-flex tw-items-center tw-border-yellow-200"></div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <ul class="timeline">
+            <li>
+              <div class="timeline-badge">
+                <i class="glyphicon glyphicon-check"></i>
+              </div>
+              <div
+                class="timeline-panel tw-rounded-sm"
+                style={{ border: "none", background: "rgb(15, 24, 46)" }}
+              >
+                <div class="timeline-heading  tw-text-white">
+                  <h4 class="timeline-title  tw-text-white tw-text-4xl">
+                    Chứng chỉ
+                  </h4>
+                  <p>
+                    <small class="text-muted">
+                      <i class="glyphicon glyphicon-time  tw-text-white"></i>{" "}
+                    </small>
+                  </p>
                 </div>
-            </div>
-<div class="container">
-
-    <ul class="timeline">
-      
-        <li>
-          <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-          <div class="timeline-panel tw-rounded-sm" style={{border: 'none', background: 'rgb(15, 24, 46)'}}>
-            <div class="timeline-heading  tw-text-white">
-              <h4 class="timeline-title  tw-text-white tw-text-4xl">Chứng chỉ</h4>
-              <p><small class="text-muted"><i class="glyphicon glyphicon-time  tw-text-white"></i> </small></p>
-            </div>
-            <div class="timeline-body  tw-text-white">
-                <ul className='tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2'>
-                    <li className='tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex'>
-                        <span className='tw-flex-1 tw-list-item'>The Web Developer Bootcamp 2024</span>
-                        <span className='tw-flex-1 tw-list-item'>Introduction to Cybersecurity (Cấp bởi Cisco)</span>
-                        <span className='tw-flex-1 tw-list-item'>Introduction to IoT  (Cấp bởi Cisco)</span>
-                        <span className='tw-flex-1 tw-list-item'>Bằng tiếng Nhật tương đương JLPT N3</span>
+                <div class="timeline-body  tw-text-white">
+                  <ul className="tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2">
+                    <li className="tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex">
+                      <span className="tw-flex-1 tw-list-item">
+                        The Web Developer Bootcamp 2024
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        Introduction to Cybersecurity (Cấp bởi Cisco)
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        Introduction to IoT (Cấp bởi Cisco)
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        Bằng tiếng Nhật tương đương JLPT N3
+                      </span>
                     </li>
-                </ul>
-            </div>
-          </div>
-        </li>
-        <li class="timeline-inverted">
-          <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
-          <div class="timeline-panel tw-rounded-sm"  style={{border: 'none', background: 'rgb(15, 24, 46)'}}>
-            <div class="timeline-heading  tw-text-white">
-              <h4 class="timeline-title tw-text-white ">Học vấn</h4>
-            </div>
-            <div class="timeline-body  tw-text-white">
-                <ul className='tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2'>
-                    <li className='tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex'>
-                        <span className='tw-flex-1 tw-list-item'>Tốt nghiệp trường đại học Công nghệ TP. HCM (HUTECH)</span>
-                        <span className='tw-flex-1 tw-list-item'>Chuyên ngành công nghệ phần mềm</span>
-                        <span className='tw-flex-1 tw-list-item'>Thời gian: 2019 - 204</span>
-                        <span className='tw-flex-1 tw-list-item'>GPA: 2.8/4.0</span>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge warning">
+                <i class="glyphicon glyphicon-credit-card"></i>
+              </div>
+              <div
+                class="timeline-panel tw-rounded-sm"
+                style={{ border: "none", background: "rgb(15, 24, 46)" }}
+              >
+                <div class="timeline-heading  tw-text-white">
+                  <h4 class="timeline-title tw-text-white ">Học vấn</h4>
+                </div>
+                <div class="timeline-body  tw-text-white">
+                  <ul className="tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2">
+                    <li className="tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex">
+                      <span className="tw-flex-1 tw-list-item">
+                        Tốt nghiệp trường đại học Công nghệ TP. HCM (HUTECH)
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        Chuyên ngành công nghệ phần mềm
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        Thời gian: 2019 - 2024
+                      </span>
+                      <span className="tw-flex-1 tw-list-item">
+                        GPA: 2.8/4.0
+                      </span>
                     </li>
-                </ul>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-          <div class="timeline-panel tw-rounded-sm" style={{border: 'none', background: 'rgb(15, 24, 46)'}}>
-            <div class="timeline-heading  tw-text-white">
-              <h4 class="timeline-title  tw-text-white tw-text-4xl">Kinh nghiệm làm việc</h4>
-              <p><small class="text-muted"><i class="glyphicon glyphicon-time  tw-text-white"></i> </small></p>
-            </div>
-            <div class="timeline-body  tw-text-white">
-                <ul className='tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2'>
-                    <li className='tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex'>
-                        <span className='tw-flex-1 tw-list-item'>Backend Developer C# - Thực tập tại Công Ty TNHH đồng hành Việt - Japan</span>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-badge">
+                <i class="glyphicon glyphicon-check"></i>
+              </div>
+              <div
+                class="timeline-panel tw-rounded-sm"
+                style={{ border: "none", background: "rgb(15, 24, 46)" }}
+              >
+                <div class="timeline-heading  tw-text-white">
+                  <h4 class="timeline-title  tw-text-white tw-text-4xl">
+                    Kinh nghiệm làm việc
+                  </h4>
+                  <p>
+                    <small class="text-muted">
+                      <i class="glyphicon glyphicon-time  tw-text-white"></i>{" "}
+                    </small>
+                  </p>
+                </div>
+                <div class="timeline-body  tw-text-white">
+                  <ul className="tw-mt-5 tw-list-disc tw-ml-5 tw-space-y-2">
+                    <li className="tw-text-white-100 tw-text-[14px] tw-tracking-wider tw-flex-col tw-flex">
+                      <span className="tw-flex-1 tw-list-item">
+                        Backend Developer C# - Thực tập tại Công Ty TNHH đồng
+                        hành Việt - Japan
+                      </span>
                     </li>
-                </ul>
-            </div>
-          </div>
-        </li>
-        
-    </ul>
-</div>
-      
+                  </ul>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-</section>
+    </section>
   );
 };
-
-
-
 
 export default Timeline;
